575c55b563f8e36a4ec440cb78b5652400128e75
belerweb <belerweb@gmail.com>
2013-04-28 15:00:12 +0800

    add get balance test

---
+++ src/test/java/com/belerweb/sms/_9nuo/SmsTest.java
@@ -0,0 +1,12 @@
+package com.belerweb.sms._9nuo;
+
+import org.junit.Test;
+
+public class SmsTest {
+
+  @Test
+  public void testGetBalance() {
+    System.out.println(Sms.init().getBalance());
+  }
+
+}
